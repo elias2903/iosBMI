a6a20d57ef2c97b7b83c225403d683b7f9a7ec82
elias <elias.saad03@gmail.com>
2020-04-10 13:33:20 +0300

    Created the project

---
+++ BMI-App/BMI-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMI-App
+//
+//  Created by elias saad on 4/10/20.
+//  Copyright © 2020 elias saad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
